1a1466b31dc76fa59678013b8ba1862087fee5d6
Andy Tang <andytang824@gmail.com>
2020-12-23 12:00:46 +1100

    Start of base core

---
+++ src/src/Entity.java
@@ -0,0 +1,29 @@
+package src;
+
+public class Entity {
+
+    private float speed;
+    private float size;
+
+    public Entity() {
+        this.speed = 1;
+        this.size = 1;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public float getSize() {
+        return size;
+    }
+
+    public void setSize(float size) {
+        this.size = size;
+    }
+
+}
---
+++ src/src/Player.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Player {
+
+    private int lifeCount;
+
+    public Player(int lifeCount) {
+        this.lifeCount = lifeCount;
+    }
+
+    public int getLifeCount() {
+        return lifeCount;
+    }
+
+    public void setLifeCount(int lifeCount) {
+        this.lifeCount = lifeCount;
+    }
+
+}
