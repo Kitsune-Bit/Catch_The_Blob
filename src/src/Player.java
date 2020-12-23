d33e97febac1eb44382a6f7be5266e84b79f75c3
Andy Tang <andytang824@gmail.com>
2020-12-23 16:09:15 +1100

    Added helper player functions and gameview

--- src/src/Player.java
+++ src/src/Player.java
@@ -1,11 +1,15 @@
 package src;
 
+import java.util.ArrayList;
+
 public class Player {
 
     private int lifeCount;
+    private ArrayList<Blob> blobs;
 
     public Player(int lifeCount) {
         this.lifeCount = lifeCount;
+        this.blobs = new ArrayList<Blob>();
     }
 
     public int getLifeCount() {
@@ -14,6 +18,24 @@ public class Player {
 
     public void setLifeCount(int lifeCount) {
         this.lifeCount = lifeCount;
+    }
+
+    public void addBlob(Blob blob) {
+        blobs.add(blob);
+    }
+
+    public boolean submitBlob(ArrayList<Blob> required) {
+        int n = blobs.size() - 1;
+        int m = required.size() - 1;
+        for (int i = 0; i < required.size(); i++) {
+            if (blobs.get(n-i) != required.get(m-i)) {
+                return false;
+            }
+        }
+        for (int i = 0; i < required.size(); i++) {
+            blobs.remove(n-i);
+        }
+        return true;
     }
 
 }
---
+++ src/view/GameViewManager.java
@@ -0,0 +1,98 @@
+package view;
+
+import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class GameViewManager {
+
+    private static final int height = 600;
+    private static final int width = 800;
+    private static final int border = 50;
+
+    private AnchorPane gameAnchorPane;
+    private Scene gameScene;
+    private Stage gameStage;
+
+    private Stage menuStage;
+    private ImageView player;
+    private boolean isLeftKeyPressed;
+    private boolean isRightKeyPressed;
+    private AnimationTimer gameTimer;
+
+    public GameViewManager() {
+        initializeStage();
+        createKeyListeners();
+    }
+
+    private void createKeyListeners() {
+
+        gameScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+        @Override
+        public void handle(KeyEvent event) {
+            if (event.getCode() == KeyCode.LEFT) {
+                isLeftKeyPressed = true;
+            } else if (event.getCode() == KeyCode.RIGHT) {
+                isRightKeyPressed = true;
+            }
+        }
+        });
+        gameScene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+        @Override
+        public void handle(KeyEvent event) {
+            if (event.getCode() == KeyCode.LEFT) {
+                isLeftKeyPressed = false;
+            } else if (event.getCode() == KeyCode.RIGHT) {
+                isRightKeyPressed = false;
+            }
+        }
+        });
+    }
+
+    private void initializeStage() {
+        gameAnchorPane = new AnchorPane();
+        gameScene = new Scene(gameAnchorPane, width, height);
+        gameStage = new Stage();
+        gameStage.setScene(gameScene);
+    }
+
+    public void createNewGame(Stage menuStage) {
+        this.menuStage = menuStage;
+        this.menuStage.hide();
+        player = new ImageView("/images/player.png");
+        player.setLayoutX(width/2);
+        player.setLayoutY(height - border);
+        gameAnchorPane.getChildren().add(player);
+        createGameLoop();
+        gameStage.show();
+    }
+
+    private void movePlayer() {
+        if (isLeftKeyPressed && !isRightKeyPressed) {
+            if(player.getLayoutX() > border) {
+                player.setLayoutX(player.getLayoutX() - 5);
+            }
+        }
+        
+        if (isRightKeyPressed && !isLeftKeyPressed) {
+            if(player.getLayoutX() < width - border) {
+                player.setLayoutX(player.getLayoutX() + 5);
+            }
+        }
+    }
+
+    private void createGameLoop() {
+        gameTimer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                movePlayer();
+            }
+        };
+        gameTimer.start();
+    }
+}
